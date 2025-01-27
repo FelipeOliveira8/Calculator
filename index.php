9d5609b8894b4f641e61eee6b195b4bd336c0119
Felipe Oliveira <fc0669493@gmail.com>
2025-01-27 14:17:49 -0300

    initial front-end finalized
    Finalized the initial version of the front-end

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
 <html>
+    <head>
+        <title> Calculator </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    <head>   
     <body>
-    </bod>
+        <section>
+            <div class="calculatorMain">
+                <div class="display">
+                    <span>0</span>
+                </div>
+                <div class="keys">
+                    <div class="keysLeft">
+                        <button class="key">( )</button>
+                        <button class="key">÷</button>
+                        <button class="key">x</button>
+                        <button class="key">7</button>
+                        <button class="key">8</button>
+                        <button class="key">9</button>
+                        <button class="key">4</button>
+                        <button class="key">5</button>
+                        <button class="key">6</button>
+                        <button class="key">1</button>
+                        <button class="key">2</button>
+                        <button class="key">3</button>
+                        <button class="key">,</button>
+                        <button class="key">0</button>
+                        <button class="key">.</button>
+                    </div>
+                    <div class="KeysRight">
+                        <button class="key clear">C</button>
+                        <button class="key">-</button>
+                        <button class="key">+</button>
+                        <button class="key result">=</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
 </html>
